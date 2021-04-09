5df8e8a0b3bebfbc91705ece7d310e264fef672c
Credators <yaroslavsev.artur@gmail.com>
2021-04-09 18:25:29 +0300

    win comm

--- block3-1.php
+++ block3-1.php
@@ -6,7 +6,7 @@ define("PSW" , "secret123");
 $tmp_login = 'admim';
 $tmp_psw = 'secret123';
 
-if ($tmp_login === LOGIN && $tmp_psw !== PSW){ // подумать над учучшением
+if ($tmp_login === LOGIN && $tmp_psw !== PSW){ 
         echo "Check correct your password";
     }elseif ($tmp_login !== LOGIN) {
         echo "User not found";
