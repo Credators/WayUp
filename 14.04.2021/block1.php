cb489a2a2ae2015d212bbb4a3608f804f33d22f9
Coda <yaroslavtsev.artur@gmail.com>
2021-04-15 13:09:17 +0300

    block1 ok

--- 14.04.2021/block1.php
+++ 14.04.2021/block1.php
@@ -1,6 +1,7 @@
 <?php
 /*Переберите и выведите данный массив на страницу в виде готового HTML (также можно использовать стили)
 с помощью циклов for, while и foreach*/
+
 $todos = [
     [
         "id" => 1,
@@ -22,10 +23,37 @@ $todos = [
     ]
 ];
 
-for($i = 0; $i <= count($todos); $i++){
-    echo $todos[$i] . "/n";
+$html = "<table border='1'><th>Номер</th><th>Наименование</th><th>Дата</th><th>Статус</th>";
+
+$html .= "<tr><td colspan='4'>Ниже задача выполнена в Foreach</td></tr>";
+
+foreach ($todos as $item){
+        $html .= "<tr><td>{$item["id"]}</td>";
+        $html .= "<td>{$item["title"]}</td>";
+        $html .= "<td>{$item["date"]}</td>";
+        $html .= "<td>{$item["status"]}</td></tr>";
+    }
+
+$html .= "<tr><td colspan='4'>Ниже задача выполнена в цикле For</td></tr>";
+
+for ($i = 0 ; $i < count($todos); $i++){
+        $html .= "<tr><td>{$todos[$i]["id"]}</td>";
+        $html .= "<td>{$todos[$i]["title"]}</td>";
+        $html .= "<td>{$todos[$i]["date"]}</td>";
+        $html .= "<td>{$todos[$i]["status"]}</td></tr>";
 }
 
-/*while($todos){
+$html .= "<tr><td colspan='4'>Ниже задача выполнена в цикле While</td></tr>";
 
-}*/
+$i = 0;
+while ($i < count($todos)){
+        $html .= "<tr><td>{$todos[$i]["id"]}</td>";
+        $html .= "<td>{$todos[$i]["title"]}</td>";
+        $html .= "<td>{$todos[$i]["date"]}</td>";
+        $html .= "<td>{$todos[$i]["status"]}</td></tr>";
+        $i++;
+};
+
+$html .= "</table>";
+
+echo $html;
