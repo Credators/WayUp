5c2b06c92ba136f21f28aa9499bdbbe275587efc
Coda <yaroslavtsev.artur@gmail.com>
2021-04-18 15:38:45 +0300

    func put_cont

--- 16.04 - $_FILES - загрузка файлов/block1/includes/add_product.php
+++ 16.04 - $_FILES - загрузка файлов/block1/includes/add_product.php
@@ -1,12 +1,17 @@
 <?php
 
+//print_r($_POST);
+//die;
+
 $form = "Название товара: {$_POST["product"]}\n";
 $form .= "Описание: {$_POST["description"]}\n";
 $form .= "Категория товара: {$_POST["category"]}\n";
 $form .= "Цена: {$_POST["price"]} грн.";
 
-$product = "products.txt";
-
-$fp = fopen($product, "w");
+/*$fp = fopen("product.txt", "w+");
 fwrite($fp, $form);
-fclose($fp);
+fclose($fp);*/
+
+file_put_contents("product.txt", $form);
+
+
--- 16.04 - $_FILES - загрузка файлов/blocl2/index.php
+++ 16.04 - $_FILES - загрузка файлов/blocl2/index.php
@@ -1,7 +1,5 @@
-
-
 <form action="includes/upload.php" method="post" enctype="multipart/form-data">
     <input type="file" name="image">
-        <p>Загрузите файл с картинкой</p>
-        <button type="submit">send</button>
+    <p>Загрузите файл с картинкой</p>
+    <input type="submit">
 </form>
