e38fa205d58cdbe33c7f6192a0cbe3643fabebe1
Credators <yaroslavsev.artur@gmail.com>
2021-04-18 13:32:15 +0300

    uniq

--- 16.04 - $_FILES - загрузка файлов/blocl2/index.php
+++
@@ -1,5 +0,0 @@
-<form action="includes/upload.php" method="post" enctype="multipart/form-data">
-    <input type="file" name="image">
-    <p>Загрузите файл с картинкой</p>
-    <input type="submit">
-</form>
